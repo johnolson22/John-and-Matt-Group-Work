09b7a6e8f2e70e439a2e975596dee500fc242f34
Matt6208 <matthew.lorenzo@bishopireton.org>
2020-03-30 13:41:22 -0400

    please work

--- File Reader/FileReader.java
+++ File Reader/FileReader.java
@@ -1,5 +1,38 @@
-public class FileReader {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+// Author: Matt Lorenzo and John Olson
+// Date: 3/23/2020
+// Exercise: Chapter 15 Ex 7
+import java.util.Scanner;
+import java.io.*;
+public class FileReader
+{
+   private int rows, cols;
+   private char pic[][];
+   
+   public FileReader(String file)
+   {
+      File f = new File(file);
+      Scanner input = null;
+      try
+      {
+         input = new Scanner(f);
+      }
+      catch(FileNotFoundException ex)
+      {
+         System.out.println("*** Cannot Open " + file + " ***");
+         System.exit(1);
+      }
+      rows = input.nextInt();
+      cols = input.nextInt();
+      pic = new char[rows][cols];
+      
+   }
+   
+   
+   
+   
+   public static void main(String[] args)
+   {
+      Scanner kb = new Scanner(System.in);
+      
+   }
 }
